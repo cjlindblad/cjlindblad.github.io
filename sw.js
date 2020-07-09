61ade894643d9a54819cab04c599f3f8186823b3
cj lindblad <carljohan.lindblad@gmail.com>
2020-07-09 21:15:35 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-59be700d7a12e9ee9e8a.js"
+    "url": "webpack-runtime-605a0875383d73d976a0.js"
   },
   {
     "url": "styles.70cfde10ad292b0c90d5.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6a5abd6fa4683f42a06916264364ef09"
+    "revision": "c59195bbc926e44be6299b05977a5453"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
